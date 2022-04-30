6c2a6209863b497f002564c848e814f23f6a49aa
qwespong <99456611+qwespong@users.noreply.github.com>
2022-04-30 13:03:32 +0100

    added helloview api

--- profilles_api/models.py
+++ profilles_api/models.py
@@ -5,47 +5,46 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
 # Create your models here.
+
+
 class UserProfileManager(BaseUserManager):
     """Manager for user profiles"""
 
-    def create_user(self,email,name,password=None):
+    def create_user(self, email, name, password=None):
         """Create a new user profile"""
         if not email:
             raise ValueError('User must have an email')
 
-        email = self.normalize_email()
-        user =  self.model(email=email,name=name)
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
 
         user.set_password(password)
         user.save(using=self._db)
 
-        return user 
-    
-    def create_superuser(self,email,name,password):
+        return user
+
+    def create_superuser(self, email, name, password):
         """The new super user with given details"""
-        user = self.create_user(email, name, password)
+        user = self.create_user(email=email, name=name, password=password)
 
         user.is_superuser = True
-        user.is_staff =True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
 
 
-
-
-class UserProfile(AbstractBaseUser,PermissionsMixin):
+class UserProfile(AbstractBaseUser, PermissionsMixin):
     """Database model for users in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default= True)
-    is_staff = models.BooleanField(default = False)
-
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UserProfileManager()
 
-    USERNAME_FIELD ='email'
-    REQUIRED_FIELD =['name']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         """Retrieve full name of user"""
@@ -54,6 +53,7 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
     def get_short_name(self):
         """Retrieve shot name"""
         return self.name
+
     def __str__(self):
         """Return String representation of user"""
         return self.email
